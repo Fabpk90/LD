2d67a6619c2197388a06e45026c0377a2cef541b
Fabpk90 <fabrizio1996@live.fr>
2020-10-03 14:53:33 +0200

    Initial commit

---
+++ Source/LD/LDGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LDGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALDGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALDGameMode();
+};
+
+
+
